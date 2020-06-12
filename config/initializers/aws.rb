701d769275dc7a6dbbabfb77de03afd644d2d99e
Gurung <rajgurung26@gmail.com>
2020-06-12 23:07:18 +0100

    [Dynamo] wip- Setup DyanoDB model. AWS config setup.

---
+++ config/initializers/aws.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+require 'dynamoid'
+
+Dynamoid.configure do |config|
+  config.access_key = ENV['aws_access_key_id']
+  config.secret_key = ENV['aws_secret_access_key']
+  config.region = ENV['aws_region']
+end
