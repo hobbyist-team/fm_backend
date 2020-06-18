37b2e261b2f1d8b9c0d6741b168e50b4ab96c6b7
Yam Shrestha <crony_yam@yahoo.com>
2020-06-18 23:33:26 +0100

    created class to read json file and loop through each record. Will be used to push record in dynamo db

---
+++ app/models/import_file.rb
@@ -0,0 +1,14 @@
+module Models
+  #
+  # ImportFile read json file and insert to dynamo db
+  #
+  class ImportFile
+    def self.insert_to_dynamo_db
+      data = ::Presenters::StreamPresenter.presents(::Models::Stream.all)
+      data.each do |item|
+        # add a new method that will insert the record to the dynamo db
+        puts "id #{item.id} title #{item.title} imageUrl #{item.imageUrl} frequency #{item.frequency}"
+      end
+    end
+  end
+end
