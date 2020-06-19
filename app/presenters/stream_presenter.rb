7ddb82e0ec53bf8536b459b9a9bb8a7bbdfdaac4
ys00065 <crony_yam@yahoo.com>
2020-06-19 23:45:29 +0100

    removed attr_reader attributes

--- app/presenters/stream_presenter.rb
+++ app/presenters/stream_presenter.rb
@@ -5,8 +5,6 @@ module Presenters
   # Presenter for Streams
   #
   class StreamPresenter
-    # attr_reader added - used by importFile model
-    attr_reader :id, :title, :image_url, :frequency
     def self.presents(data)
       data.map do |obj|
         new(
@@ -22,7 +20,7 @@ module Presenters
     def initialize(id, title, image_url, frequency)
       @id = id
       @title = title
-      @image_url = image_url
+      @imageUrl = image_url # rubocop:disable Naming/VariableName
       @frequency = frequency
     end
   end
